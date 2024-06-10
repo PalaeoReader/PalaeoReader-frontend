c084345c69a53bb86a7df39a960756b14032374c
roma <romajsarathi23@gmail.com>
2024-06-10 15:30:16 -0400

    added more functions

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@ document.body.innerHTML = '<div id="app"></div>';
 // Render your React component instead
 const root = createRoot(document.getElementById('app'));
 root.render(<h1>Hello, world</h1>);
+
+//window.addEventListener("load");
+
+function Submit() {
+  
+  return (
+  <button>Submit</button>
+)}
+
+function App() {
+  return (
+ <Submit />
+  );
+}
+
+root.render(<App />);
