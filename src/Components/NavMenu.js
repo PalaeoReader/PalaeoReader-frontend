f46b49a970b500c0c65dbe606c199b2376f23c47
roma <romajsarathi23@gmail.com>
2024-06-13 15:49:25 -0400

    some changes to NavMenu and css

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const NavMenu = () => {
     return (
-        <div className = "Navlinks">
-        <a>home</a>
-        <a>directory</a>
-        <a>map</a>
-        <a>about us</a>
-        <a>contact</a>
-        <a>log in</a>
+        <div className = "NavHeader">
+            <div className = "Navlinks">
+                <a>Home</a>
+                <a>Directory</a>
+                <a>Map</a>
+                <a>About us</a>
+                <a>Contact</a>
+                <a>Log in</a>
+            </div>
         </div>
     );
 }
