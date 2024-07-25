7319a540622c2c669276202fd206627b2fea94dc
roma <romajsarathi23@gmail.com>
2024-07-25 14:46:44 -0400

    minor css changes

--- src/Components/ArtifactDirectory/ArtifactRow.js
+++ src/Components/ArtifactDirectory/ArtifactRow.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ArtifactMetadata from '../GetArtifact';
 import useSWR from 'swr';
 import { APIgetArtifacts } from '../../APIurls';
