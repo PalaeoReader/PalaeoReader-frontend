2ae4fa47c297de2f4a280a100bd069908ae9b792
Jonathan Washington <jonathan.n.washington@gmail.com>
2024-08-02 16:39:39 -0400

    updates to morphs stuff

--- src/APIurls.js
+++ src/APIurls.js
@@ -6,6 +6,7 @@ const APIgetArtifactImageGrp = 'http://localhost:8000/api/images/groups/'
 const APIgetArtifactContentSets = 'http://localhost:8000/api/content/sets/'
 const APIgetImage = 'http://localhost:8000/api/images/'
 const APIgetMorphGroup = 'http://localhost:8000/api/content/morphs/groups/'
+const APIgetMorphOccurrences = 'http://localhost:8000/api/search/morphs/'
 //const APIgetArtifactContentSources = ''
 
 export {
@@ -14,5 +15,6 @@ export {
     APIgetArtifacts,
     APIgetArtifactContentSets,
     APIgetImage,
-    APIgetMorphGroup
+    APIgetMorphGroup,
+	 APIgetMorphOccurrences
 }
--- src/Components/Morphs.js
+++ src/Components/Morphs.js
@@ -2,23 +2,18 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import useSWR from 'swr';
 //import './Selections.css';
-import { APIgetMorphGroup } from '../APIurls';
+import { APIgetMorphGroup, APIgetMorphOccurrences } from '../APIurls';
+import './Morphs.css';
 
-const fetcher = (APIgetMorphGroup) => fetch(APIgetMorphGroup).then((res) => res.json());
+const MGfetcher = (APIgetMorphGroup) => fetch(APIgetMorphGroup).then((res) => res.json());
+const MOfetcher = (APIgetMorphOccurrences) => fetch(APIgetMorphOccurrences).then((res) => res.json());
 
-export default function MorphGroup () {
-	const { id: mgid } = useParams();
 
-	const {
-		data: morphGroupData,
-		error: mgError,
-		isValidating: mgIsValidating
-	} = useSWR(APIgetMorphGroup+mgid, fetcher);
+function MorphGroupHeader (props) {
 
-	if (mgError) return <div className="failed">failed to load</div>;
-	if (mgIsValidating) return <div className="loading">Loading...</div>;
+	const morphGroupData = props.data;
 
-	return (
+	const MorphGroupHeader = () => (
 		<div>
 			<h2>{morphGroupData.name}</h2>
 
@@ -26,5 +21,111 @@ export default function MorphGroup () {
 			<p>{morphGroupData.description}</p>
 		</div>
 	)
+
+	return <MorphGroupHeader/>
+}
+
+
+function MorphOccurrenceTokenType (props) {
+	//console.log(props.type)
+	return (
+		<>
+			{props.type.text}
+		</>
+	)
+}
+
+function MorphOccurrenceToken (props) {
+	console.log(props.highlight, props.idx, props.token)
+	let spanClasses;
+	if (props.idx==props.highlight) {
+		spanClasses="token highlighted";
+	} else {
+		spanClasses="token";
+	}
+	//if (props.highlight)
+	return (
+		<>
+		<span className={spanClasses}>
+		{props.token.types.map(type => (
+			<>
+			{type.type === 'original' ? <MorphOccurrenceTokenType type={type} /> : ""
+			}
+			</>
+		))}
+		</span>&nbsp;
+		</>
+	)
+}
+
+function MorphOccurrenceLine (props) {
+	//console.log(props);
+	return (
+		<div className="morph-line">
+		{props.line.tokens.map((token, idx) => (
+			<MorphOccurrenceToken token={token} idx={idx} highlight={props.line.selected_token_index} />
+		))}
+		</div>
+	)
+}
+
+function MorphOccurrences (props) {
+
+/*	const morphOccurrence = morphOccurrences.map((imageList, imageListId) => (
+		
+	));
+
+	return MorphOccurrences
+	*/
+
+	const morphOccurrenceData = props.data;
+	//console.log(morphOccurrenceData);
+
+/*	const MorphOccurrences = (
+		{morphOccurrenceData.map(({line}, index) => (
+		<MorphOccurrenceLine line={line} />
+		))}
+	);
+
+	return <MorphOccurrences/>
+	*/
+	return (
+		<>
+		<h2>Occurrences</h2>
+		{morphOccurrenceData.map(line => (
+			<MorphOccurrenceLine line={line}/>
+		))}
+		</>
+	);
+}
+
+
+export default function MorphGroup () {
+
+	const { id: mgid } = useParams();
+
+	const {
+		data: morphGroupData,
+		error: mgError,
+		isValidating: mgIsValidating
+	} = useSWR(APIgetMorphGroup+mgid, MGfetcher);
+
+
+	const {
+		data: morphOccurrenceData,
+		error: moError,
+		isValidating: moIsValidating
+	} = useSWR(APIgetMorphOccurrences+mgid+"?window_span=5", MOfetcher);
+
+	if (mgError || moError) return <div className="failed">failed to load</div>;
+	if (mgIsValidating || moIsValidating) return <div className="loading">Loading...</div>;
+
+
+	return (
+		<>
+		<MorphGroupHeader data={morphGroupData} />
+		<MorphOccurrences data={morphOccurrenceData} />
+		</>
+	);
 
 }
